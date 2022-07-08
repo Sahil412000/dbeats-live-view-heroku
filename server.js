49f5ba3415446f688a9094c560abe2272bae99e2
Sahil412000 <sahilpanjabi123@gmail.com>
2022-07-08 15:02:33 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const port = process.env.PORT || 8000;
+const cors = require("cors");
+
+// const corsOptions = {
+//   origin: "*",
+//   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+// };
+
+// app.use(cors(corsOptions));
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "https://beta.dbeats.live",
+    methods: ["GET", "POST"],
+    transports: ["websocket", "polling"],
+    credentials: true,
+  },
+});
+
+//app.use(cors(corsOptions));
+
+// app.options('*', cors(corsOptions));
+
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: [
+//       "http://localhost:*",
+//       "https://beta.dbeats.live ",
+//       "https://dbeats.live ",
+//     ],
+//     transports: ["websocket", "polling"],
+//     credentials: true,
+//     allowedHeaders: ["Access-Control-Allow-Origin"],
+//     methods: ["GET", "POST", "PUT"],
+//   },
+//   allowEIO3: true,
+// });
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Live Viewer Working!");
+});
+
+io.on("connection", (socket) => {
+  let roomId;
+
+  socket.on("joinlivestream", async (room) => {
+    await socket.join(room.toString());
+    roomId = room;
+    console.log(room);
+    console.log(io.sockets.adapter.rooms.get(room).size);
+    let roomSize = io.sockets.adapter.rooms.get(room).size;
+    let details = {
+      room: room,
+      roomSize: roomSize,
+    };
+    socket.emit("count", details);
+    io.to(room).emit("livecount", details);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+    if (roomId != undefined) {
+      let roomSize = io.sockets.adapter.rooms.get(roomId).size;
+      io.to(roomId).emit("removecount", roomSize);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
